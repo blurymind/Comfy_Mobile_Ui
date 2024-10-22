bf69a733294ff0f2d5475b0a5d38b8fc38d6bf19
blurymind <blurymind@gmail.com>
2024-10-22 19:09:38 +0100

    fix tags regression

--- frontend/src/components/PromptEditor.tsx
+++ frontend/src/components/PromptEditor.tsx
@@ -11,6 +11,8 @@ export interface Props {
 	setBookmarkedPrompts: any;
 }
 
+export const toSelectPropList = (input: Array<string>) => input.map((value:any)=> ({value, label:value}))
+export const fromSelectPropList  = (input: Array<{value:string,label:string}>) => input.map((item:any)=> item.value)
 export default ({
 	onChange,
 	value,
@@ -20,40 +22,25 @@ export default ({
 }: Props) => {
 	const selectRef = useRef(null);
 	const [tabIndex, setTabIndex] = useLocalStorage("promptTabIndex", 0);
-	// const [selectOptions, setSelectOptions] = useLocalStorage<Array<any>>(
-	// 	"selectOptions",
-	// 	[],
-	// );
-	const [availableTags, setAvailableTags] = useState<any>(new Set([]))
-	const [promptTags, setPromptTags] = useLocalStorage<Array<any>>("promptTags", []);
+	const [availablePromptTags, setAvailablePromptTags] = useLocalStorage<Array<string>>("availablePromptTags", []);// can be selected
+	const [promptTags, setPromptTags] = useLocalStorage<Array<string>>("promptTags", []);// selected now
 	const [textPrompt, setTextPrompt] = useLocalStorage('defaultPromptValue',defaultPromptValue);
 	const [isTagsPinned, setIsTagsPinned] = useLocalStorage("tagsMenuOpen", true);
-
-	// console.log({ defaultPromptValue });
-	const unusedTags = useMemo(() => {
-		const splitValues = value.split(',').map(item=> item.trim())
-		
-		const result = promptTags.filter(
-			(option) => !splitValues.includes(option.value.trim()),// todo value is the combined result of the prompt
-		);
-		console.log({splitValues, result, promptTags})
-		return result;
-	}, [value, promptTags]);
 
 	// console.log({ isTagsPinned, unusedTags });
 	useEffect(() => {
 		console.log({ promptTags, textPrompt });
-		const combinedPrompt = `${promptTags.map((item: any) => item.value).join(",")}, ${textPrompt}`;
-		console.log({ combinedPrompt, value });
-		setAvailableTags((prev: any) => ([...prev, textPrompt?.split(',').map(item=>item.trim())]))
+		const combinedPrompt = `${promptTags.join(",")}, ${textPrompt}`;
 		onChange(combinedPrompt);
 	}, [promptTags, textPrompt]);
 
-	const onSetNewTags = (newOptions: Array<any>) => {
-		const newUniqueTagOptions = newOptions.filter(newItem => !promptTags.some(option=> option.value === newItem.value) )
+	const onSetNewTags = (newOptions: Array<string>) => {
+		const newUniqueTagOptions = newOptions.filter(newItem => !promptTags.some(option=> option === newItem))
 		setPromptTags([...promptTags, ...newUniqueTagOptions])
+		const newAvailablePromptTagsNoDups = new Set([...availablePromptTags,...promptTags, ...newUniqueTagOptions])
+		setAvailablePromptTags(Array.from(newAvailablePromptTagsNoDups))
 	}
-	const onCreateNew = (newOption: any) => {
+	const onCreateNew = (newOption: string) => {
 		// console.log('-- create new tag -->',{ newOption });
 		// const hasOption = selectOptions.find(
 		// 	(item) => item.value === newOption.value,
@@ -61,6 +48,7 @@ export default ({
 		// // console.log({ newOption, hasOption });
 		// if (hasOption) return;
 		// setSelectOptions((prev) => [...prev, newOption]);
+		console.log({newOption})
 		onSetNewTags([newOption])
 	};
 
@@ -80,11 +68,11 @@ export default ({
 				// console.log({item, props, state, methods, itemIndex})
 				const onSelectItem = () => {
 					//@ts-ignore
-					setPromptTags((prev: any) => [...prev, item]);
+					setPromptTags((prev: any) => [...prev, item.value]);
 				};
 				const onDeleteTag = () => {
 					setPromptTags((prev) =>
-						prev.filter((option) => option.value !== item.value),
+						prev.filter((option) => option !== item.value),
 					);
 				};
 				return (
@@ -94,7 +82,7 @@ export default ({
 					</div>
 				);
 			},
-		[setPromptTags, setPromptTags],
+		[setPromptTags],
 	);
 
 	const BookmarkSelector = useMemo(() => {
@@ -102,33 +90,32 @@ export default ({
 			.map((key) => ({ label: key, value: bookmarkedPrompts[key] }))
 			.concat([{ value: "", label: " Add bookmark" }]);
 		console.log({ bookmarkedPromptsOptions, bookmarkedPrompts, value });
+
+		const onChange = (newSelected: any) => {
+			console.log({ newSelected, promptTags });
+			if (!newSelected[0]) {
+				const ask = prompt("Create a new bookmark?", "my bookmark");
+				return;
+			}
+			const newTags = newSelected[0].value?.tags
+				?.split(",")
+				
+			
+			if (newTags) {
+				console.log('--- set new tags', {newTags})
+				onSetNewTags(newTags);
+			}
+		}
 		return (
 			<div className="bookmakrs-selector">
 				<Select
 					options={bookmarkedPromptsOptions}
-					onChange={(newSelected: any) => {
-						console.log({ newSelected, promptTags });
-						if (!newSelected[0]) {
-							const ask = prompt("Create a new bookmark?", "my bookmark");
-							return;
-						}
-						const newTags = newSelected[0].value?.tags
-							?.split(",")
-							?.map((value: string) => ({ value, label: value }));
-						
-						if (newTags) {
-							console.log('--- set new tags', {newTags})
-							setPromptTags(newTags);
-							// onSetNewTags(newTags);
-							// setSelectOptions((prev) => [...prev, ...newTags]);
-						}
-						// onSetCurrentWorkflowData(newSelected[0].value);
-					}}
+					onChange={onChange}
 					contentRenderer={() => <div title="bookamrks">Bookmarks</div>}
 					optionRenderer={(item: any) => (
 						<div
 						key={`${item.value}`}
-							onClick={console.log}
+							onClick={()=> onChange([{value: {tags: item.value.tags,}}])}
 							// style={{width: '300px'}}
 							// className={`flex spaced ${item.value === currentCollection ? "react-dropdown-select-item-selected" : ""}`}
 						>
@@ -143,7 +130,9 @@ export default ({
 			</div>
 		);
 	}, [bookmarkedPrompts, setPromptTags, ]);
-	console.log({ value, unusedTags, availableTags})
+	const promptTagsProp: any = toSelectPropList(promptTags)
+	console.log({ value, promptTagsProp, availablePromptTags})
+
 	return (
 		<div
 			className="flex padded-x prompt-editor"
@@ -169,21 +158,18 @@ export default ({
 							// style={{overflow:"auto"}}
 						>
 							<Select
-								options={unusedTags}
-								onCreateNew={onCreateNew}
+								options={toSelectPropList(availablePromptTags.filter(item => !promptTags.includes(item)))}
+								onCreateNew={(item: any)=> onCreateNew(item.value)}
 								create
-								// labelField="name"
-								// valueField="id"
 								ref={selectRef}
 								keepOpen={isTagsPinned}
-								// closeOnSelect={!isTagsPinned}
 								dropdownHandle={isTagsPinned}
 								multi
-								values={promptTags}
+								values={promptTagsProp}
 								onChange={(newTags) => {
 									console.log('---- NEW TAGS SET ', {newTags})
 									setIsTagsPinned(false);
-									setPromptTags(newTags as any);
+									setPromptTags(fromSelectPropList(newTags));
 								}}
 								clearable
 								searchable
